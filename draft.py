f3e187c0916ca4a3aefbacf05e5be7992d9842fd
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-11-22 12:22:15 +0000

    Tidied up basic error handling for http request

--- draft.py
+++ draft.py
@@ -1,11 +1,14 @@
 import requests
 import json
 
-resp = requests.get('https://api.pushshift.io/reddit/search/comment/?q=dems&subreddit=The_Donald&metadata=true&size=2')
+# The pushshift api only alows a size of < 500 responses per request
+resp = requests.get('https://api.pushshift.io/reddit/search/comment/?q=dems&subreddit=The_Donald&metadata=true&size=499')
 if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /reddit/search/comment  {}'.format(resp.status_code))
+    # Just die if we don't get what we want. Dramatic I know.
+    raise requests.HTTPError('GET /reddit/search/comment  {}'.format(resp.status_code))
+    os._exit(1)
 
+# Parse the body of each comment
 jsonresponse = resp.json()["data"]
 for val in jsonresponse:
     print(val["body"])
