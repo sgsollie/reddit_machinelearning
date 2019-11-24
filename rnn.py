edae63ac6e1dcccca4b113bc056a96ec070ceba7
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-11-24 23:17:47 +0000

    Added missing import

--- rnn.py
+++ rnn.py
@@ -2,6 +2,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
+from keras.layers import TimeDistributed
 import numpy as np
 import tensorflow
 
